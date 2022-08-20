faddd21b4a8353cdc02e965679cd0175ed1566ce
Sagi <sagi403@gmail.com>
2022-08-20 14:52:00 +0300

    Fetching model data

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,9 @@
 import { User } from "./models/User";
 
-const user = new User({ name: "myName", age: 20 });
+const user = new User({ id: 1 });
 
-user.on("change", () => {
-  console.log("Change #1");
-});
-user.on("change", () => {
-  console.log("Change #2");
-});
-user.on("save", () => {
-  console.log("Save was triggered");
-});
+user.fetch();
 
-user.trigger("change");
-user.trigger("save");
-user.trigger("notValidEvent");
+setTimeout(() => {
+  console.log(user);
+}, 4000);
