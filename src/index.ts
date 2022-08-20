f4c52c8467e18fd7296c12a83ae416f2fd9c5fac
Sagi <sagi403@gmail.com>
2022-08-20 13:40:00 +0300

    Dynamic array creation

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import { User } from "./models/User";
 
 const user = new User({ name: "myName", age: 20 });
 
-user.set({ name: "newName" });
+user.on("change", () => {});
+user.on("change", () => {});
+user.on("checkClick", () => {});
 
-console.log(user.get("name"));
-console.log(user.get("age"));
+console.log(user);
