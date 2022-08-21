772e125a877995ca8100fe3309868e8d338f8b40
Sagi <sagi403@gmail.com>
2022-08-21 18:04:21 +0300

    An advanced generic constraint

--- src/models/Attributes.ts
+++ src/models/Attributes.ts
@@ -1,7 +1,7 @@
 export class Attributes<T extends object> {
   constructor(private data: T) {}
 
-  get(key: string): number | string {
+  get<K extends keyof T>(key: K): T[K] {
     return this.data[key];
   }
 
