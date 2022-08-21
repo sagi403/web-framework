e7cc848ba9d6562858a80880a9133a08f83a21af
Sagi <sagi403@gmail.com>
2022-08-21 18:11:12 +0300

    Re-integrating attributes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { User } from "./models/User";
 
-const user = new User();
+const user = new User({ name: "new record", age: 0 });
 
 user.events.on("change", () => {
   console.log("change!");
